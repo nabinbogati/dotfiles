9d8f769b285a2457ac80b99f084e40804075647e
nabinbogati <nabin31bogati@gmail.com>
2020-08-21 17:57:53 +0545

    renamed commands.txt -> remove_decoration.sh

--- remove_decoration.sh
+++ remove_decoration.sh
@@ -1,5 +1,5 @@
-# remove all windows decoration buttons in gnome3.36 ( best with popos + tiling )
+# remove all windows decoration buttons(Gnome 3.36)
 gsettings set org.gnome.desktop.wm.preferences button-layout :
 
-# change default terminal to alacritty in gnome3.36
+# change default terminal to alacritty(Gnome 3.36)
 gsettings set org.gnome.desktop.default-applications.terminal exec alacritty:
