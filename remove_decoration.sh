dfda4e12262237f56787b5d2127eaaec6ecb2e14
nabinbogati <nabin31bogati@gmail.com>
2020-08-29 19:52:12 +0545

    combine remove_decoration to setup.sh

--- remove_decoration.sh
+++
@@ -1,5 +0,0 @@
-# remove all windows decoration buttons(Gnome 3.36)
-gsettings set org.gnome.desktop.wm.preferences button-layout :
-
-# change default terminal to alacritty(Gnome 3.36)
-gsettings set org.gnome.desktop.default-applications.terminal exec alacritty:
--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# remove all windows decoration buttons(Gnome 3.36)
+gsettings set org.gnome.desktop.wm.preferences button-layout :
+
+# change default terminal to alacritty(Gnome 3.36)
+gsettings set org.gnome.desktop.default-applications.terminal exec alacritty:
+
 
 check_availablity() {
     args="$@"
