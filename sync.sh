23e4b70995d8372e0b25be007da777db80a7a65f
nabinbogati <nabin31bogati@gmail.com>
2020-08-29 19:51:30 +0545

    combine rsync and rbackup to sync.sh

--- sync.sh
+++ sync.sh
@@ -3,16 +3,16 @@
 # backup local folders to remotedrive
 log_file="$(pwd)/rsync.log"
 
-rclone -q copy --update ~/GoogleDrive google-drive-dev: 2>> "$log_file"
+rclone -q copy --update ~/GoogleDrive google-drive-dev: 2>> "$log_file" && rclone -q sync google-drive-dev: ~/GoogleDrive 2>> "$log_file"
 
 line="-----------------------------------------------------------------"
 
 if [ $? -eq 0 ];then
     echo $line
-    echo "Backup Successful"
+    echo "Backup and Sync Successful"
     echo $line
 else
     echo $line
-    echo "Backup Failed. See $log_file for more details."
+    echo "Operation Failed. See $log_file for more details."
     echo $line
 fi
