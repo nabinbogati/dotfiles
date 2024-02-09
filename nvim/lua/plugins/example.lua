fade9103451e1744f689d249e1f603c9672669ef
Nabin Boati <nabin31bogati@gmail.com>
2024-02-09 02:34:48 +0000

    sticky scrolling

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -11,6 +11,9 @@ if true then
       "nvim-telescope/telescope-file-browser.nvim",
       dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
     },
+    -- {
+    --   "nvim-treesitter/nvim-treesitter-context",
+    -- }
   }
 end
 
