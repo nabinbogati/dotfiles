1604f369bd3b07b1b2c047f782b92ee8f20452be
nabinbogati <nabin31bogati@gmail.com>
2024-09-01 11:14:05 +0545

    minor updates

--- zsh/pyenv.sh
+++ zsh/pyenv.sh
@@ -5,16 +5,16 @@ sudo apt-get install python3-pip
 curl https://pyenv.run | bash
 
 # clone virtualenvwrapper for pyenv
-git clone https://github.com/pyenv/pyenv-virtualenvwrapper.git $(pyenv root)/plugins/pyenv-virtualenvwrapper
+git clone https://github.com/pyenv/pyenv-virtualenvwrapper.git $(~/.pyenv/bin/pyenv root)/plugins/pyenv-virtualenvwrapper
 
-echo '' >> ~/.zshrc
-echo '# pyenv settings' >> ~/.zshrc
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-echo 'eval "$(pyenv init -)"' >> ~/.zshrc
-echo 'export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENT="true"' >> ~/.zshrc
-echo 'export PATH="~/.local/bin:$PATH"' >> ~/.zshrc
-echo 'pyenv virtualenvwrapper_lazy' >> ~/.zshrc
+echo '' >>~/.zshrc
+echo '# pyenv settings' >>~/.zshrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >>~/.zshrc
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >>~/.zshrc
+echo 'eval "$(pyenv init -)"' >>~/.zshrc
+echo 'export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENT="true"' >>~/.zshrc
+echo 'export PATH="~/.local/bin:$PATH"' >>~/.zshrc
+echo 'pyenv virtualenvwrapper_lazy' >>~/.zshrc
 
 ### Install python
 [[ -d ~/.pyenv/versions/3.10.2 ]] || ~/.pyenv/bin/pyenv install 3.10.2
