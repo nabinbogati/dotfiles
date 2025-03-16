2d642e1475845b57553aaa3a5e2a22282f69b514
nabinbogati <nabin31bogati@gmail.com>
2025-03-16 11:26:00 +0545

    added cheat.sh support and virtualenv fixes

--- zsh/pyenv.sh
+++ zsh/pyenv.sh
@@ -7,18 +7,17 @@ curl https://pyenv.run | bash
 # clone virtualenvwrapper for pyenv
 git clone https://github.com/pyenv/pyenv-virtualenvwrapper.git $(~/.pyenv/bin/pyenv root)/plugins/pyenv-virtualenvwrapper
 
-echo '' >>~/.zshrc
-echo '# pyenv settings' >>~/.zshrc
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >>~/.zshrc
-echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >>~/.zshrc
-echo 'eval "$(pyenv init -)"' >>~/.zshrc
-echo 'export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENT="true"' >>~/.zshrc
-echo 'export PATH="~/.local/bin:$PATH"' >>~/.zshrc
-echo 'pyenv virtualenvwrapper_lazy' >>~/.zshrc
+# echo '# pyenv settings' >>~/.zshrc
+# echo 'export PYENV_ROOT="$HOME/.pyenv"' >>~/.zshrc
+# echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >>~/.zshrc
+# echo 'eval "$(pyenv init -)"' >>~/.zshrc
+# echo 'export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENT="true"' >>~/.zshrc
+# echo 'export PATH="~/.local/bin:$PATH"' >>~/.zshrc
+# echo 'pyenv virtualenvwrapper_lazy' >>~/.zshrc
 
 ### Install python
 [[ -d ~/.pyenv/versions/3.10.2 ]] || ~/.pyenv/bin/pyenv install 3.10.2
-pyenv global 3.10.2
+~/.pyenv/bin/pyenv global 3.10.2
 
 # install virtualenvwrapper from pip
 pip install virtualenvwrapper
